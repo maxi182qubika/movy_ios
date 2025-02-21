9b17cde7fcfd5e53a26ecb38b19d2766a1cccf2f
maxi <Maxim.Ferraiuo@partner.vas.com>
2025-02-21 10:50:15 -0300

    Initial Commit

---
+++ movy/movyApp.swift
@@ -0,0 +1,17 @@
+//
+//  movyApp.swift
+//  movy
+//
+//  Created by Maximiliano Ferraiuolo on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct movyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
